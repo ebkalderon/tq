6968432771ec7383316d6fabc5c4288355ad700f
Eyal Kalderon <ebkalderon@gmail.com>
2018-08-31 20:42:11 +0800

    Implement debug for Filter, add cur_step field

--- src/filter.rs
+++ src/filter.rs
@@ -1,3 +1,5 @@
+use std::fmt::{Debug, Formatter, Result as FmtResult};
+
 use toml::Value;
 
 #[derive(Debug)]
@@ -11,13 +13,27 @@ pub enum SearchStep<'a> {
 pub struct Filter<'s, 't> {
     steps: &'s [SearchStep<'s>],
     values: &'t mut Iterator<Item = Option<Value>>,
+    cur_step: usize,
+}
+
+impl<'s, 't> Debug for Filter<'s, 't> {
+    fn fmt(&self, fmt: &mut Formatter) -> FmtResult {
+        fmt.debug_struct("Filter")
+            .field("steps", &self.steps)
+            .field("cur_step", &self.cur_step)
+            .finish()
+    }
 }
 
 impl<'s, 't> Filter<'s, 't> {
-    pub fn new<V: Iterator<Item = Option<Value>>>(
-        steps: &'s [SearchStep],
-        values: &'t mut V,
-    ) -> Self {
-        Filter { steps, values }
+    pub fn new<V>(steps: &'s [SearchStep], values: &'t mut V) -> Self
+    where
+        V: Iterator<Item = Option<Value>>,
+    {
+        Filter {
+            steps,
+            values,
+            cur_step: 0,
+        }
     }
 }
