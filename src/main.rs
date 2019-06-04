844176a13537dba37d9c5ac921b62a7fa841230c
Eyal Kalderon <ebkalderon@gmail.com>
2019-06-05 04:04:20 +0800

    Print Debug and Display representations of AST in main.rs

--- src/main.rs
+++ src/main.rs
@@ -7,5 +7,6 @@ use tq::parser::parse_filter;
 fn main() {
     let f =
         parse_filter(&env::args().nth(1).unwrap_or(".".into())).expect("Failed to parse filter");
-    println!("{:?}", f);
+    println!("AST: {:?}", f);
+    println!("Serialized: {}", f);
 }
