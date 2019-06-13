5c898ac3eab43d9a8d7177b5c01cdcb1e91f2c38
Eyal Kalderon <ebkalderon@gmail.com>
2019-06-13 23:27:15 +0800

    Use structopt for input parsing in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,37 @@
 #![forbid(unsafe_code)]
 
-use std::env;
+use std::path::PathBuf;
+use std::process;
 
+use structopt::StructOpt;
 use tq::parser::parse_filter;
 
+#[derive(Debug, StructOpt)]
+struct Opt {
+    #[structopt(default_value = ".", parse(from_str = "read_filter"))]
+    pub filter: String,
+    #[structopt(default_value = "Vec::new")]
+    pub files: Vec<PathBuf>,
+}
+
+fn read_filter(s: &str) -> String {
+    if s.is_empty() {
+        ".".to_string()
+    } else {
+        s.to_string()
+    }
+}
+
 fn main() {
-    let f =
-        parse_filter(&env::args().nth(1).unwrap_or(".".into())).expect("Failed to parse filter");
-    println!("AST: {:?}", f);
-    println!("Serialized: {}", f);
+    let opt = Opt::from_args();
+    let filter = match parse_filter(&opt.filter) {
+        Ok(filter) => filter,
+        Err(err) => {
+            eprintln!("{}", err);
+            process::exit(1);
+        }
+    };
+
+    println!("AST: {}", filter);
+    println!("Serialized: {:?}", filter);
 }
