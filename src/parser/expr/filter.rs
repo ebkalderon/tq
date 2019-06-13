aec2f37246df64425900a4c0949a2bb298d23101
Eyal Kalderon <ebkalderon@gmail.com>
2019-06-13 20:16:09 +0800

    Refactor first_segment() for clarity

--- src/parser/expr/filter.rs
+++ src/parser/expr/filter.rs
@@ -8,8 +8,7 @@ pub fn filter<'a>() -> Parser<'a, u8, Expr> {
     let identity = sym(b'.').map(|_| ExprFilter::Identity);
     let recurse = seq(b"..").map(|_| ExprFilter::Recurse);
 
-    let first = field() | (sym(b'.') * index().map(ExprFilter::Index));
-    let segments = first_segment(first) + (field() | index().map(ExprFilter::Index)).repeat(0..);
+    let segments = first_segment() + (field() | index().map(ExprFilter::Index)).repeat(0..);
     let path = segments.map(|(first, rest)| {
         rest.into_iter().fold(first, |prev, next| {
             ExprFilter::Path(Box::new(prev), Box::new(next))
@@ -19,13 +18,16 @@ pub fn filter<'a>() -> Parser<'a, u8, Expr> {
     (recurse | path | identity).map(|e| Expr::Filter(Box::new(e)))
 }
 
-fn first_segment<'a>(filter: Parser<'a, u8, ExprFilter>) -> Parser<'a, u8, ExprFilter> {
-    let opt_prefix = variable().map(ExprFilter::Variable).opt();
-    let first = opt_prefix + filter;
-    first.map(|(prefix, filter)| match prefix {
-        Some(prefix) => ExprFilter::Path(Box::new(prefix), Box::new(filter)),
-        None => filter,
-    })
+fn first_segment<'a>() -> Parser<'a, u8, ExprFilter> {
+    let identity_field = field();
+    let identity_index = sym(b'.') * index().map(ExprFilter::Index);
+    let identity = identity_field | identity_index;
+
+    let var_field = variable().map(ExprFilter::Variable) + field();
+    let var_index = variable().map(ExprFilter::Variable) + index().map(ExprFilter::Index);
+    let variable = var_field | var_index;
+
+    identity | variable.map(|(var, seg)| ExprFilter::Path(Box::new(var), Box::new(seg)))
 }
 
 fn field<'a>() -> Parser<'a, u8, ExprFilter> {
