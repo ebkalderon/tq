8820853bcf12d0a88876cffaf3ce490d5be67616
Eyal Kalderon <ebkalderon@gmail.com>
2019-06-15 14:45:15 +0800

    Disallow reserved keywords in identifier paths and function names

---
+++ src/parser/tokens/keywords.rs
@@ -0,0 +1,36 @@
+use pom::parser::*;
+
+macro_rules! define_keywords {
+    ($($function:ident => $keyword:tt),+) => {
+        pub fn keyword<'a>() -> Parser<'a, u8, &'a [u8]> {
+            $($function())|+
+        }
+
+        $(
+            pub fn $function<'a>() -> Parser<'a, u8, &'a [u8]> {
+                seq(stringify!($keyword).as_bytes())
+            }
+        )+
+    };
+}
+
+define_keywords! {
+    keyword_and => and,
+    keyword_as => as,
+    keyword_break => break,
+    keyword_catch => catch,
+    keyword_def => def,
+    keyword_elif => elif,
+    keyword_end => end,
+    keyword_empty => empty,
+    keyword_foreach => foreach,
+    keyword_if => if,
+    keyword_import => import,
+    keyword_include => include,
+    keyword_label => label,
+    keyword_module => module,
+    keyword_or => or,
+    keyword_reduce => reduce,
+    keyword_then => then,
+    keyword_try => try
+}
